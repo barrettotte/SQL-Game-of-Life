a8ff9e9b075d12cd359f1714d71a729ae14749a7
barrettotte <barrettotte@gmail.com>
2019-09-18 15:49:35 -0400

    Started looping over generations

--- Game-of-life.sql
+++ Game-of-life.sql
@@ -2,70 +2,137 @@
 -- https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life --
 
 /*
+Rules:
 	1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 	2. Any live cell with two or three live neighbours lives on to the next generation.
 	3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 	4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+Improvements:
+   - Maybe improve performance by only writing data around each automata ?
+   - Move @AllGenerations to an actual table so it can be queried in the future
+   - Table creation SQL for @AllGenerations when moved to table
+   - Separate stored proc to seed data from JSON or flat file (assert @IterationStart > 0)
 */
 
 
--- TODO: Create table from generation 0 seed data (flat file or JSON) --
+-- TODO: Convert this to a stored procedure
+----- PARAMS START -----
+DECLARE @SizeX            INT = 25;
+DECLARE @SizeY            INT = 25;
+DECLARE @IterationStart   INT =  0;
+DECLARE @IterationEnd     INT =  2;
+----- PARAMS END -------
 
 
--- TODO: Accept this as a stored proc param
-DECLARE @SizeX INT;
-SET @SizeX = 25;
+-- Variables --
+DECLARE @CurrentIteration   INT = @IterationStart;
+DECLARE @AllGenerations     TABLE (Id BIGINT, Generation INT, X INT, Y INT, Alive BIT DEFAULT(0), Neighbors SMALLINT);
+DECLARE @BlankGeneration    TABLE (Id BIGINT, Generation INT, X INT, Y INT, Alive BIT DEFAULT(0), Neighbors SMALLINT);
 
-DECLARE @SizeY INT;
-SET @SizeY = 25;
-
-DECLARE @Iterations INT;
-SET @Iterations = 3;
-
-DECLARE @CurrentIteration INT;
-SET @CurrentIteration = 1;
+DECLARE @IdIter BIGINT = 0;
+DECLARE @I INT;
+DECLARE @J INT;
 
 
--- Store each generation --
-DECLARE @AllGenerations		TABLE (Generation INT NOT NULL, X INT NOT NULL, Y INT NOT NULL, Alive BIT NOT NULL DEFAULT(1));
-DECLARE @CurrentGeneration	TABLE (Generation INT NOT NULL, X INT NOT NULL, Y INT NOT NULL, Alive BIT NOT NULL DEFAULT(1));
-DECLARE @LastGeneration		TABLE (Generation INT NOT NULL, X INT NOT NULL, Y INT NOT NULL, Alive BIT NOT NULL DEFAULT(1));
-DECLARE @BlankGeneration	TABLE (Generation INT NOT NULL, X INT NOT NULL, Y INT NOT NULL, Alive BIT NOT NULL DEFAULT(1));
-
-
--- Create a blank "template" to reuse later -- 
-DECLARE @I INT = 0, @J INT = 0;
-WHILE(@I < @SizeX) BEGIN
-	WHILE(@J < @SizeY) BEGIN
-		INSERT INTO @BlankGeneration (Generation, X, Y, Alive) VALUES (0, @I, @J, 0);
-		SET @J = @J + 1
-	END
-	SET @I = @I + 1
-	SET @J = 0
-END
-
-
--- Seed generation 0
-INSERT INTO @AllGenerations SELECT * FROM @BlankGeneration;
-UPDATE @AllGenerations SET Alive = 1 WHERE Generation = 0 AND X = 1 AND Y = 2;  --[ 
-UPDATE @AllGenerations SET Alive = 1 WHERE Generation = 0 AND X = 1 AND Y = 3;	--   Blinker 
-UPDATE @AllGenerations SET Alive = 1 WHERE Generation = 0 AND X = 0 AND Y = 3;  --]
-
-
--- 
-WHILE(@CurrentIteration < @Iterations) BEGIN
-	UPDATE @BlankGeneration SET Generation = @CurrentIteration;
-	INSERT INTO @AllGenerations SELECT * FROM @BlankGeneration;
-	SET @CurrentIteration = @CurrentIteration + 1
+-- Create a blank "template" to reuse each generation --
+BEGIN
+	SET @IdIter = 0;
+	SET @I = 0;
+	SET @J = 0;
+	WHILE(@I < @SizeX) BEGIN
+		WHILE(@J < @SizeY) BEGIN
+			INSERT INTO @BlankGeneration (Id, Generation, X, Y, Alive, Neighbors) VALUES (@IdIter, 0, @I, @J, 0, 0);
+			SET @J = @J + 1;
+			SET @IdIter = @IdIter + 1;
+		END;
+		SET @I = @I + 1;
+		SET @J = 0;
+	END;
 END;
 
 
-SELECT * FROM @AllGenerations;
+
+-- Seed generation 0 --
+IF @CurrentIteration = 0 BEGIN
+	INSERT INTO @AllGenerations SELECT * FROM @BlankGeneration;
+
+	-- Blinker --
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 1 AND Y = 0;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 1 AND Y = 1;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 1 AND Y = 2;
+
+	-- Glider --
+	/*
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 1 AND Y = 0;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 2 AND Y = 1;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 0 AND Y = 2;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 1 AND Y = 2;
+	UPDATE @AllGenerations SET Alive = 1 WHERE Generation = @CurrentIteration AND X = 2 AND Y = 2;
+	*/
+
+	SET @CurrentIteration = 1;
+END;
 
 
+-- Game of life loop --
+WHILE(@CurrentIteration < @IterationEnd) BEGIN
+	SET @IdIter = 0;
+	SET @I = 0;
+	SET @J = 0;
+	DECLARE @CurrentGeneration  TABLE (Id BIGINT, Generation INT, X INT, Y INT, Alive BIT DEFAULT(0), Neighbors SMALLINT);
+	DECLARE @PreviousGeneration TABLE (Id BIGINT, Generation INT, X INT, Y INT, Alive BIT DEFAULT(0), Neighbors SMALLINT);
+	DECLARE @Alives TABLE (Id BIGINT, Alive SMALLINT);
+	DECLARE @NeighborCounts TABLE (Id BIGINT, Neighbors SMALLINT);
+
+	/*
+	BEGIN
+		IF (@CurrentIteration = 1)
+			INSERT INTO @PreviousGeneration SELECT * FROM @BlankGeneration;	
+		ELSE
+			INSERT INTO @PreviousGeneration SELECT * FROM @AllGenerations WHERE Id = @IdIter - 1;
+	END;
+	*/
+	
+	INSERT INTO @CurrentGeneration SELECT * FROM @AllGenerations WHERE Generation=@CurrentIteration-1;
+	
+	UPDATE @CurrentGeneration SET Generation = @CurrentIteration;
 
 
--- TODO: Return X and Y bounds as out parameters (for screen drawing at correct size)
+	WHILE(@I < @SizeX) BEGIN
+		WHILE(@J < @SizeY) BEGIN
+			DECLARE @NeighborCount SMALLINT = 0;
+			DECLARE @IsAlive BIT = (SELECT Alive FROM @CurrentGeneration WHERE Id = @IdIter);
+
+			IF @IsAlive = 1 BEGIN
+				SET @NeighborCount = 1; -- test
+			END;
+
+			INSERT INTO @NeighborCounts (Id, Neighbors) VALUES (@IdIter, @NeighborCount);
+			SET @J = @J + 1;
+			SET @IdIter = @IdIter + 1;
+		END;
+		SET @I = @I + 1;
+		SET @J = 0;
+	END;
+
+	-- Add current generation data to all generations --
+	INSERT INTO @AllGenerations
+		SELECT 
+			NC.Id, Generation, X, Y, Alive, NC.Neighbors 
+		FROM 
+			@NeighborCounts NC
+		FULL JOIN
+			@CurrentGeneration CG
+		ON CG.Id = NC.Id
+	;
+
+
+	SET @CurrentIteration = @CurrentIteration + 1;
+END;
+
+
+SELECT * FROM @AllGenerations WHERE Id < 50;
 
 
 
