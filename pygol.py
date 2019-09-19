360dc3497e90ce145016b671832d336fef43ff21
barrettotte <barrettotte@gmail.com>
2019-09-19 09:43:13 -0400

    slight change to test impl

--- pygol.py
+++ pygol.py
@@ -1,38 +1,42 @@
-# Test implementation of game of life in python
-# https://www.geeksforgeeks.org/conways-game-life-python-implementation/
+# Test implementation of Game of Life
+# https://nostarch.com/download/samples/PythonPlayground_sampleCh3.pdf
 
-import numpy as np 
-import matplotlib.pyplot as plt  
-import matplotlib.animation as animation   
-  
-def update(f, img, grid, size):  
-    newGrid = grid.copy() 
-    for i in range(size): 
-        for j in range(size): 
-            total = int((
-                grid[i, (j-1)%size]          + grid[i, (j+1)%size] + 
-                grid[(i-1)%size, j]          + grid[(i+1)%size, j] + 
-                grid[(i-1)%size, (j-1)%size] + grid[(i-1)%size, (j+1)%size] + 
-                grid[(i+1)%size, (j-1)%size] + grid[(i+1)%size, (j+1)%size]
-            )/255) 
-            if grid[i, j] == 255:
-                if total < 2 or total > 3: newGrid[i, j] = 0
-            else: 
-                if total == 3: newGrid[i, j] = 255
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+def randomGrid(N): return np.random.choice([255,0], N*N, p=[0.2, 0.8]).reshape(N, N)
+
+def addGlider(i, j, grid): grid[i:i+3, j:j+3] = np.array([[0, 0, 255], [255, 0, 255], [0, 255, 255]])
+
+def update(frameNum, img, grid, N):
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            if grid[i, j]  == 255:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = 0
+            else:
+                if total == 3:
+                    newGrid[i, j] = 255
     img.set_data(newGrid)
     grid[:] = newGrid[:]
-    return img
+    return img,
 
-def main(): 
-    size = 30
+def main():
+    N = 25
     updateInterval = 250
-   
-    grid = np.zeros(size * size).reshape(size, size) 
-    grid[3,1]=255 ; grid[1,2]=255 ; grid[3,2]=255 ; grid[2,3]=255 ; grid[3,3]=255 # Add glider
-  
-    fig, ax = plt.subplots() 
-    img = ax.imshow(grid, interpolation='nearest') 
-    frame = animation.FuncAnimation(fig, update, fargs=(img, grid, size), frames=10, interval=updateInterval, save_count=50) 
-    plt.show() 
-  
-if __name__ == '__main__': main() 
+    grid = np.array([])
+    grid = np.zeros(N*N).reshape(N, N)
+    addGlider(1, 1, grid)
+    #grid = randomGrid(N)
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ), frames=10, interval=updateInterval, save_count=50)
+    plt.show()
+
+if __name__ == '__main__': main()
